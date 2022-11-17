4ce33b4eb73fa343e6eb287736793b574b5fa368
Sachin Kumar <91049993+sachinkumar1469@users.noreply.github.com>
2022-11-18 05:20:48 +0530

    Add files via upload

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,12 +1,42 @@
 import React from 'react'
-import memeData from './data';
+// import memeData from './data';
 
 const Meme = ()=>{
-    const [memeUrl,setMemeUrl] = React.useState("");
     
+    const [memeData,setMemeData] = React.useState([])
+    const [meme,setMeme] = React.useState({
+            topText:"",
+            bottomText:"",
+            memeUrl:""
+        }
+    );
+    React.useEffect(()=>{
+        
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res=>res.json())
+            .then(result=>setMemeData(result.data.memes))
+    },[])
+    function handleChange(e){
+        const {name,value} = e.target;
+        console.log(name,value);
+        setMeme((prevMeme)=>{
+            return({
+                ...prevMeme,
+                [name]:value
+            })
+        })
+        
+    }
     function handleClick(event) {
         event.preventDefault();
-        setMemeUrl(memeData['data']['memes'][randomNumber()].url);
+        setMeme((prevMeme)=>{
+            return (
+                {
+                    ...prevMeme,
+                    memeUrl : memeData[randomNumber()].url
+                }
+            )
+        });
     }
     
     function randomNumber(){
@@ -17,13 +47,15 @@ const Meme = ()=>{
         <div className='meme-container'>
             <form>
                 <div className='input-wrapper'>
-                    <input type='text' placeholder='top text'/>
-                    <input type='text' placeholder='bottom text'/>
+                    <input type='text' placeholder='top text' name='topText' value={meme.topText} onChange={handleChange}/>
+                    <input type='text' placeholder='bottom text' name='bottomText' value={meme.bottomText} onChange={handleChange}/>
                 </div>
                 <button onClick={handleClick} className='form-btn'>Get a new meme Image</button>
             </form>
             <div className='image-wrapper'>
-                {memeUrl && <img src={memeUrl} alt="alt" />}
+                {meme.memeUrl && <img src={meme.memeUrl} alt="alt" />}
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
             </div>
         </div>
     )
